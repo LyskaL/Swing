713f5688fc1223d7d17464a48b1cdd0fe53da69f
Lyuda Lyska <lucy.daehyun@gmail.com>
2016-11-29 18:59:58 +0200

    Implementation of the third example from module 1. Create a simple timer.

---
+++ src/rin2016/module1/StopWatch.java
@@ -0,0 +1,58 @@
+package rin2016.module1;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class StopWatch implements ActionListener {
+	private JLabel _jlab;
+	private long _start;
+	
+	public StopWatch() {
+		JFrame jfrm = new JFrame("StopWatch");
+		jfrm.setLayout(new FlowLayout());
+		jfrm.setSize(220, 95);
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton jbtnStart = new JButton("Start");
+		JButton jbtnStop = new JButton("Stop");
+		//Связывание с кнопками обработчиков событий действия
+		jbtnStart.addActionListener(this);
+		jbtnStop.addActionListener(this);
+		jfrm.add(jbtnStart);
+		jfrm.add(jbtnStop);
+		//Создание метки и привязка её в фрейму
+		_jlab = new JLabel("Press Start to begin timing.");
+		jfrm.add(_jlab);
+		jfrm.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(final ActionEvent ae) {
+		//Получении текущего системного времени
+		Calendar cal = Calendar.getInstance(); 
+		if(ae.getActionCommand().equals("Start")) {
+			if (_start == 0) {
+				//Сохранение времени запуска
+				_start = cal.getTimeInMillis();
+				_jlab.setText("Stop watch is Running...");
+			}
+		} else {
+			if (_start != 0) {
+				int sec = (int)(cal.getTimeInMillis()-_start)/1000;
+				int mill = (int)(cal.getTimeInMillis()-_start)-(1000*sec);
+				//Вычисляем время прошедшее от запуска до остановки
+				_jlab.setText("Elapsed time is " + sec + "." + mill );
+				_start = 0;
+			}
+		}
+	}
+	
+	public static void main(final String[] args) {
+		//Создание фрейма в потоке обработки событий
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						new StopWatch();
+					}
+				});
+	}
+}
