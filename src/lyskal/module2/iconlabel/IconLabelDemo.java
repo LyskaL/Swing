de636e1a46e6ddfa5db498e6566223c9d1d06784
LyskaL <lucy.daehyun@gmail.com>
2016-12-06 15:20:02 +0200

    In the second example, the second module changed manager layout (GridBagLayout).

--- src/lyskal/module2/iconlabel/IconLabelDemo.java
+++ src/lyskal/module2/iconlabel/IconLabelDemo.java
@@ -8,13 +8,14 @@ public class IconLabelDemo {
 	
 	IconLabelDemo() {
 		JFrame jfrm = new JFrame("My window");
-		jfrm.setLayout(new GridLayout());
+		jfrm.setLayout(new GridBagLayout());
 		jfrm.setSize(350, 300);
 		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		_myIcon = new ImageIcon("src//lyskal//module2//iconlabel//Icon.jpg");
-		//Creates a label containing text and an image.
-		JLabel jlabIconText = new JLabel("Added a photo", _myIcon, SwingConstants.LEFT);
-		jfrm.add(jlabIconText);
+		JLabel jlabIcon = new JLabel(_myIcon);
+		JLabel jlabText = new JLabel("Added a photo", SwingConstants.CENTER);
+		jfrm.add(jlabText);
+		jfrm.add(jlabIcon);
 		jfrm.setVisible(true);
 	}
 	
