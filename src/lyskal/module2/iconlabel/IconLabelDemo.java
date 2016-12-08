cbb8e8bb865304f79c9fe7fa08e6445b16e74fe0
LyskaL <lucy.daehyun@gmail.com>
2016-12-08 22:15:23 +0200

    Changed the method main. Deleted call SwingUtilities.

--- src/lyskal/module2/iconlabel/IconLabelDemo.java
+++ src/lyskal/module2/iconlabel/IconLabelDemo.java
@@ -4,29 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class IconLabelDemo {
-	private ImageIcon _myIcon;
 	
-	IconLabelDemo() {
+	public static void main(final String[] args) {
 		JFrame jfrm = new JFrame("My window");
 		jfrm.setLayout(new GridBagLayout());
 		jfrm.setSize(350, 300);
 		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		_myIcon = new ImageIcon("src//lyskal//module2//iconlabel//Icon.jpg");
+		ImageIcon _myIcon = new ImageIcon("src//lyskal//module2//iconlabel//Icon.jpg");
 		JLabel jlabIcon = new JLabel(_myIcon);
 		JLabel jlabText = new JLabel("Added a photo", SwingConstants.CENTER);
 		jfrm.add(jlabText);
 		jfrm.add(jlabIcon);
 		jfrm.setVisible(true);
-	}
-	
-	public static void main(final String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				new IconLabelDemo();
-				
-			}
-		});
 	}
 }
