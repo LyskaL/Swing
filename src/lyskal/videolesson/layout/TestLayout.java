bbf0ce8a61b15df3c5dfeba33a1c61e0938c23c6
LyskaL <lucy.daehyun@gmail.com>
2016-12-14 22:45:34 +0200

    mathematical symbols are taken out in constants.

--- src/lyskal/videolesson/layout/TestLayout.java
+++ src/lyskal/videolesson/layout/TestLayout.java
@@ -9,7 +9,13 @@ public class TestLayout {
 	static JPanel jPanel = new JPanel();
 	static JPanel text = new JPanel();
 	static JPanel buttons = new JPanel();
-	
+
+	static final String PLUS = "+";
+	static final String MINUS = "-";
+	static final String MULTIPLY = "*";
+	static final String DIVISION = "/";
+	static final String EQUALS = "=";
+
 	public static void main(final String[] args) {
 		jFrame.add(jPanel);
 		jPanel.setLayout(new GridLayout(2, 0));
@@ -26,17 +32,17 @@ public class TestLayout {
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		return jFrame;
 	}
-	
+
 	@SuppressWarnings("serial")
 	static class ButtonAction extends AbstractAction {
 		@Override
 		public void actionPerformed(final ActionEvent ae) {
-			if(ae.getActionCommand().equals("=")) {
+			if (ae.getActionCommand().equals(EQUALS)) {
 				jFrame.setTitle("");
 			} else {
-				StringBuilder text = new StringBuilder(jFrame.getTitle());
-				text.append(ae.getActionCommand());
-				jFrame.setTitle(text.toString());
+				StringBuilder textTitle = new StringBuilder(jFrame.getTitle());
+				textTitle.append(ae.getActionCommand());
+				jFrame.setTitle(textTitle.toString());
 			}
 		}
 	}
@@ -48,49 +54,49 @@ public class TestLayout {
 		eight.addActionListener(new ButtonAction());
 		JButton nine = new JButton("9");
 		nine.addActionListener(new ButtonAction());
-		JButton button2 = new JButton("/");
-		button2.addActionListener(new ButtonAction());
-		
+		JButton division = new JButton(DIVISION);
+		division.addActionListener(new ButtonAction());
+
 		buttons.add(seven);
 		buttons.add(eight);
 		buttons.add(nine);
-		buttons.add(button2);
-		
+		buttons.add(division);
+
 		JButton four = new JButton("4");
 		four.addActionListener(new ButtonAction());
 		JButton five = new JButton("5");
 		five.addActionListener(new ButtonAction());
 		JButton six = new JButton("6");
 		six.addActionListener(new ButtonAction());
-		JButton button1 = new JButton("*");
-		button1.addActionListener(new ButtonAction());
+		JButton multiply = new JButton(MULTIPLY);
+		multiply.addActionListener(new ButtonAction());
 
 		buttons.add(four);
 		buttons.add(five);
 		buttons.add(six);
-		buttons.add(button1);
-		
+		buttons.add(multiply);
+
 		JButton one = new JButton("1");
 		one.addActionListener(new ButtonAction());
 		JButton two = new JButton("2");
 		two.addActionListener(new ButtonAction());
 		JButton three = new JButton("3");
 		three.addActionListener(new ButtonAction());
-		JButton minus = new JButton("-");
+		JButton minus = new JButton(MINUS);
 		minus.addActionListener(new ButtonAction());
 
 		buttons.add(one);
 		buttons.add(two);
 		buttons.add(three);
 		buttons.add(minus);
-		
+
 		JButton zero = new JButton("0");
 		zero.addActionListener(new ButtonAction());
 		JButton point = new JButton(".");
 		point.addActionListener(new ButtonAction());
-		JButton equals = new JButton("=");
+		JButton equals = new JButton(EQUALS);
 		equals.addActionListener(new ButtonAction());
-		JButton plus = new JButton("+");
+		JButton plus = new JButton(PLUS);
 		plus.addActionListener(new ButtonAction());
 		
 		buttons.add(zero);
