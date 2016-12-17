9b9ba2e1895f1a1c88050b91eec7e8cdddbd6ee9
LyskaL <lucy.daehyun@gmail.com>
2016-12-17 17:40:14 +0200

    Implementation the method creation Color Dialog.
    The panel changes color on chosen from the Color Dialog.

--- src/lyskal/videolesson/dialog/FileAndColorDialogs.java
+++ src/lyskal/videolesson/dialog/FileAndColorDialogs.java
@@ -1,5 +1,6 @@
 package lyskal.videolesson.dialog;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,13 +9,13 @@ import javax.swing.*;
 public class FileAndColorDialogs {
 	static JFrame jFrame = getJFrame();
 	static JPanel filePanel = new JPanel();
-	
+	static JPanel colorPanel = new JPanel();
 	
 	public static void main(final String[] args) {
 		createFilePanel();
+		createColorPanel();
 		filePanel.revalidate();
 	}
-	
 	
 	public static JFrame getJFrame() {
 		JFrame jFrame = new JFrame();
@@ -46,5 +47,21 @@ public class FileAndColorDialogs {
 		});
 		filePanel.add(showDialog);
 		jFrame.add(filePanel);
+	}
+	
+	public static void createColorPanel() {
+		colorPanel.setLayout(new GridLayout(3, 0));
+		colorPanel.add(new JLabel());
+		colorPanel.add(new JLabel("Choose new color of the panel"));
+		JButton showColorDialog = new JButton("color");
+		showColorDialog.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				Color color = JColorChooser.showDialog(colorPanel, "Choose color", Color.blue);
+				colorPanel.setBackground(color);
+			}
+		});
+		colorPanel.add(showColorDialog);
+		jFrame.add(colorPanel);
 	}
 }
